6e3899aa274536a1c5ecf83108a36c2efc888f01
Olga Vasiuk <i9173196099@gmail.com>
2020-01-19 17:43:02 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,6 +1,23 @@
-function teenager(age) { if (age >= 13 && age <= 19) { return "You are a teenager!"; } else {return "You are not a teenager";} }
-function ageChecker(age) { if (age >= 13 && age <= 19) { return "You are a teenager!"; } else if (age <= 12) { return "You are a kid"; } else {return "You are a grownup";} }
-function ternaryTeenager(age) { if (age >= 13 && age <= 19) { return "You are a teenager"; } else {return "You are not a teenager";} }
+function basicTeenager(age) {
+if (age >= 13 && age <= 19) {
+return "You are a teenager!";
+}
+}
+function teenager(age)
+{ if (age >= 13 && age <= 19)
+  { return "You are a teenager!"; }
+else {return "You are not a teenager";} }
+
+function ageChecker(age)
+{ if (age >= 13 && age <= 19)
+  { return "You are a teenager!"; }
+  else if (age <= 12) { return "You are a kid"; }
+  else {return "You are a grownup";} }
+
+function ternaryTeenager(age)
+{ if (age >= 13 && age <= 19)
+   { return "You are a teenager"; }
+    else {return "You are not a teenager";} }
 function switchAge(age) { switch(age) { case 13: return "You are a teenager"
 case 14: return "You are a teenager"
 case 15: return "You are a teenager"
